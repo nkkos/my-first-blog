a3552831a9a38b1f08c6b2d1e651265ebc4519c9
Nadia <me@example.com>
2016-03-07 00:03:23 +0300

    Added authentification

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import include, url
 from . import views
+from django.contrib import admin
+
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'})
 ]
